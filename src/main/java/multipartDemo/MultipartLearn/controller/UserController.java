198bb63ca9f91cf0fe70fff7e4dd7b19603293d1
ashish kumar <nihalsingh.ns48@gmail.com>
2026-07-18 11:51:29 +0530

    Enhance multipart file upload configuration and update user creation response format and cors fix

--- src/main/java/multipartDemo/MultipartLearn/controller/UserController.java
+++ src/main/java/multipartDemo/MultipartLearn/controller/UserController.java
@@ -5,19 +5,16 @@ import multipartDemo.MultipartLearn.dto.UserResponse;
 import multipartDemo.MultipartLearn.entity.User;
 import multipartDemo.MultipartLearn.repository.UserRepository;
 import multipartDemo.MultipartLearn.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import java.util.Optional;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
@@ -29,16 +26,18 @@ public class UserController {
 
 
 
-    @PostMapping( consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<String> createUser(@RequestPart("request") UserRequest request,
-                                           @RequestPart("photo") MultipartFile photo,
-                                           @RequestPart("aadhaar") MultipartFile aadhaar) {
-        try {
-            userService.save(request, photo, aadhaar);
-            return ResponseEntity.ok("User created successfully");
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body("Error creating user: " + e.getMessage());
-        }
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Map<String, String>> createUser(
+            @RequestPart("request") UserRequest request,
+            @RequestPart("photo") MultipartFile photo,
+            @RequestPart("aadhaar") MultipartFile aadhaar) throws IOException {
+
+        userService.save(request, photo, aadhaar);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "User created successfully");
+
+        return ResponseEntity.ok(response);
     }
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable Long id) {
